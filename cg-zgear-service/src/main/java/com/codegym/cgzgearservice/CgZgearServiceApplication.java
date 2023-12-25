3c231c7d4122a443e474b137d80a50e65a0f583e
MinhThang2709 <minhthang270920@gmail.com>
2023-12-25 15:30:15 +0700

    first commit

---
+++ cg-zgear-service/src/main/java/com/codegym/cgzgearservice/CgZgearServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.cgzgearservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgZgearServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgZgearServiceApplication.class, args);
+    }
+
+}
