7bad81b1eae494c8627a9e2562c5a1313a30d7cc
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2023-12-27 22:03:16 +0700

    creating BE structure and entities

---
+++ src/main/java/com/codegym/cgzgearservice/model/entitiy/product/ProductDetail.java
@@ -0,0 +1,27 @@
+package com.codegym.cgzgearservice.model.entitiy.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="product_details")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(mappedBy="detail")
+    Product product;
+
+}
---
+++ src/main/java/com/codegym/cgzgearservice/model/entitiy/user/Address.java
@@ -0,0 +1,50 @@
+package com.codegym.cgzgearservice.model.entitiy.user;
+
+import com.codegym.cgzgearservice.model.entitiy.product.Order;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="addresses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="street")
+    @NotBlank
+    private String street;
+
+    @Column(name="city")
+    @NotBlank
+    private String city;
+
+    @Column(name="district")
+    @NotBlank
+    private String district;
+
+    @Column(name="ward")
+    @NotBlank
+    private String ward;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "BIT default false")
+    private Boolean isDeleted;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = true)
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name="oder_id", nullable = true)
+    Order oder;
+
+
+}
