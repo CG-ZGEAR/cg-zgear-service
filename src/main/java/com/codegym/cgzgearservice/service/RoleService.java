385efaabf5a2ec0ad6a1e29d50225bf2713bbc6e
MinhThang2709 <minhthang270920@gmail.com>
2024-01-02 11:42:37 +0700

    Add package ( paging - formartter - security )

--- src/main/java/com/codegym/cgzgearservice/service/RoleService.java
+++ src/main/java/com/codegym/cgzgearservice/service/RoleService.java
@@ -1,4 +1,12 @@
 package com.codegym.cgzgearservice.service;
 
+import com.codegym.cgzgearservice.dto.RoleDTO;
+
+import java.util.Optional;
+
 public interface RoleService {
+    Iterable<RoleDTO> findAll();
+    Optional<RoleDTO> findById(Long id);
+    void save(RoleDTO roleDto);
+    void remove(Long id);
 }
---
+++ src/main/java/com/codegym/cgzgearservice/service/impl/SercurityServiceImpl.java
@@ -0,0 +1,4 @@
+package com.codegym.cgzgearservice.service.impl;
+
+public class SercurityServiceImpl {
+}
