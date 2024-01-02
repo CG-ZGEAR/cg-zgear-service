30ab5f62dbdafc49f4830e215dca5bb0d85075ee
Tình <126749282+TRANHUUTINH@users.noreply.github.com>
2024-01-02 20:13:41 +0700

    ghhhg

--- src/main/java/com/codegym/cgzgearservice/repository/UserRepository.java
+++ src/main/java/com/codegym/cgzgearservice/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.codegym.cgzgearservice.repository;
 
+import com.codegym.cgzgearservice.dto.UserDTO;
 import com.codegym.cgzgearservice.model.entitiy.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
+
+    List<User> findByIsDeletedTrue();
+
+    List<User> findByIsDeletedFalse();
 }
