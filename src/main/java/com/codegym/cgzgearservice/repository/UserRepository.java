9cbb16821c964dc16cb95ead817a49c94c482c96
Tình <126749282+TRANHUUTINH@users.noreply.github.com>
2024-01-02 16:41:43 +0700

    no message

--- src/main/java/com/codegym/cgzgearservice/repository/UserRepository.java
+++ src/main/java/com/codegym/cgzgearservice/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.codegym.cgzgearservice.repository;
 import com.codegym.cgzgearservice.model.entitiy.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 /**
@@ -11,8 +11,8 @@ import java.util.List;
  */
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRoles_Name(String roleName);
-=======
-public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsByUsername(String username);
->>>>>>> 3c010e10aec9ab88510650abf87db59638fd1117
 }
+
+
--- src/main/java/com/codegym/cgzgearservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/codegym/cgzgearservice/service/impl/UserServiceImpl.java
@@ -1,52 +1,6 @@
 package com.codegym.cgzgearservice.service.impl;
 
-<<<<<<< HEAD
-import com.codegym.cgzgearservice.model.entitiy.user.Role;
-import com.codegym.cgzgearservice.model.entitiy.user.User;
-import com.codegym.cgzgearservice.repository.UserRepository;
-import com.codegym.cgzgearservice.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.Set;
-
-@Service
-public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public List<User> getUsersWithUserRole() {
-        List<User> userList = userRepository.findByRoles_Name("ROLE_USER");
-        return userList;
-    }
-
-    @Override
-    public void deleteUser(Long userId, Set<Role> userRoles) {
-        User userToDelete = userRepository.findById(userId).orElse(null);
-
-        if (userToDelete == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with ID: " + userId);
-        }
-
-        boolean isAdmin = userRoles.stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN"));
-
-        if (isAdmin) {
-            userToDelete.setDeleted(true);
-            userRepository.save(userToDelete);
-        } else {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User does not have permission to delete");
-        }
-
-    }
-
-=======
 
 import com.codegym.cgzgearservice.dto.UserDTO;
 import com.codegym.cgzgearservice.model.entitiy.user.Role;
@@ -56,9 +10,12 @@ import com.codegym.cgzgearservice.repository.UserRepository;
 import com.codegym.cgzgearservice.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -74,9 +31,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private RoleRepository roleRepository;
 
-    public UserServiceImpl(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @Override
     public UserDTO registerUser(UserDTO userDTO) {
         User user = modelMapper.map(userDTO, User.class);
@@ -112,9 +70,28 @@ public class UserServiceImpl implements UserService {
         return dto;
     }
 
+
+    public List<User> getUsersWithUserRole() {
+        List<User> userList = userRepository.findByRoles_Name("ROLE_USER");
+        return userList;
+    }
+
     @Override
-    public void deleteUser(Long userId) {
+    public void deleteUser(Long userId, Set<Role> userRoles) {
+        User userToDelete = userRepository.findById(userId).orElse(null);
+
+        if (userToDelete == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with ID: " + userId);
+        }
+
+        boolean isAdmin = userRoles.stream().anyMatch(role -> role.getName().equals("ROLE_ADMIN"));
+
+        if (isAdmin) {
+            userToDelete.setDeleted(true);
+            userRepository.save(userToDelete);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User does not have permission to delete");
+        }
 
     }
->>>>>>> 3c010e10aec9ab88510650abf87db59638fd1117
 }
